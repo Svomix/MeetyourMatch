77401bae164e6e3d5cd810b9cc653b25e8c5fe39
KOT BACbKA <vagenet5122@gmail.com>
2024-11-18 17:53:17 +0400

    remap auth to api/

--- core/src/main/java/com/javanostra/spring/core/controllers/AuthController.java
+++ core/src/main/java/com/javanostra/spring/core/controllers/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/register")
+@RequestMapping("/api/register")
 @AllArgsConstructor
 public class AuthController {
 
